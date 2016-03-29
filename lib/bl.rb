bb3a28c94643ce8ead46478c16aeaeb39f8010e1
saki <sakihet@gmail.com>
2016-03-30 03:21:30 +0900

    add list command

--- lib/bl.rb
+++ lib/bl.rb
@@ -1,4 +1,5 @@
 require "thor"
+require "backlog_kit"
 require "bl/version"
 
 module Bl
@@ -6,6 +7,31 @@ module Bl
     desc "version", "show version"
     def version
       puts Bl::VERSION
+    end
+
+    desc "list", "list issues"
+    def list
+      issues = Bl::CLI.client.get('issues').body.each do |i|
+        puts [
+          i.issueType.name,
+          i.issueKey,
+          i.summary,
+          i.priority.name,
+          i.created,
+          i.dueDate,
+          i.updated,
+          i.createdUser.name,
+          i.assignee.name,
+          i.status.name
+        ].join("\t")
+      end
+    end
+
+    def self.client
+      BacklogKit::Client.new(
+        space_id: ENV['BACKLOG_SPACE_ID'],
+        api_key: ENV['BACKLOG_API_KEY']
+      )
     end
   end
 end

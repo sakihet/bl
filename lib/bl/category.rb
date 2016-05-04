8f32b1dfa2336eb80570d36731fd2c29a71155c9
saki <sakihet@gmail.com>
2016-05-04 21:34:03 +0900

    add category add/delete commands

--- lib/bl/category.rb
+++ lib/bl/category.rb
@@ -1,4 +1,28 @@
 module Bl
   class Category < Thor
+    def initialize(*)
+      @config = YAML.load_file(File.join(Dir.home, Bl::CONFIG_FILE))
+      @client = BacklogKit::Client.new(
+        space_id: @config[:space_id],
+        api_key: @config[:api_key]
+      )
+      super
+    end
+
+    desc 'add NAME', 'add categories'
+    def add(*names)
+      names.each do |name|
+        res = @client.post("projects/#{@config[:project_key]}/categories", name: name)
+        puts "category added: #{res.body.id}\t#{res.body.name}"
+      end
+    end
+
+    desc 'delete CATEGORY_ID', 'delete categories'
+    def delete(*ids)
+      ids.each do |id|
+        res = @client.delete("projects/#{@config[:project_key]}/categories/#{id}")
+        puts "category deleted: #{res.body.id}\t#{res.body.name}"
+      end
+    end
   end
 end

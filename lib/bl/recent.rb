20a6ffb707a75d16cc566a996c940b721ac5575e
saki <sakihet@gmail.com>
2016-05-12 07:19:22 +0900

    update description in recent issues command

--- lib/bl/recent.rb
+++ lib/bl/recent.rb
@@ -7,7 +7,7 @@ module Bl
       super
     end
 
-    desc 'issues COUNT', 'list recent issues'
+    desc 'issues COUNT', 'list recently viewed issues'
     def issues(count=nil)
       client.get('users/myself/recentlyViewedIssues', count: count).body.each do |i|
         puts [i.issue.issueKey, i.issue.summary].join("\t")

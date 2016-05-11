a3d291e3d6ca25bf23e7c644c913444de6482044
saki <sakihet@gmail.com>
2016-05-12 07:14:03 +0900

    rename: recent issue -> recent issues

--- lib/bl/recent.rb
+++ lib/bl/recent.rb
@@ -7,8 +7,8 @@ module Bl
       super
     end
 
-    desc 'issue COUNT', 'list recent issues'
-    def issue(count=nil)
+    desc 'issues COUNT', 'list recent issues'
+    def issues(count=nil)
       client.get('users/myself/recentlyViewedIssues', count: count).body.each do |i|
         puts [i.issue.issueKey, i.issue.summary].join("\t")
       end

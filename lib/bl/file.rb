4877e80c3356dbc4b743c410739ecc1698dbcef7
saki <sakihet@gmail.com>
2016-06-04 22:22:28 +0900

    add file get command

--- lib/bl/file.rb
+++ lib/bl/file.rb
@@ -13,5 +13,16 @@ module Bl
         puts [f.id, f.type, f.dir, f.name, f.size, f.created, f.updated].join("\t")
       end
     end
+
+    desc 'get [ID...]', 'get files'
+    def get(*ids)
+      ids.each do |id|
+        res = client.get("projects/#{@config[:project_key]}/files/#{id}")
+        f = ::File.new(res.body.filename, "w")
+        f.write(res.body.content)
+        f.close
+        puts "file #{id} #{res.body.filename} downloaded."
+      end
+    end
   end
 end

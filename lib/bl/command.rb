4437bca2dde7f6acebc6547d7a3bc127667bc0d4
saki <sakihet@gmail.com>
2017-04-16 10:39:14 +0900

    add delete_class_options

--- lib/bl/command.rb
+++ lib/bl/command.rb
@@ -3,5 +3,13 @@ module Bl
     include Bl::Requestable
     include Bl::Formatting
     class_option :format, type: :string, default: 'table', desc: 'set output format'
+
+    protected
+
+    def delete_class_options(h)
+      opts = ['format']
+      opts.map { |opt| h.delete(opt) }
+      h
+    end
   end
 end

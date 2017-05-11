f23ba5c85adee2527d07662cdd48fb0d9fd573eb
saki <sakihet@gmail.com>
2017-05-12 08:45:01 +0900

    add Bl::Requestable#request

--- lib/bl/requestable.rb
+++ lib/bl/requestable.rb
@@ -12,5 +12,22 @@ module Bl
     def formatter
       @formatter ||= Formatter.new(format: options[:format])
     end
+
+    def request(method, url, opts = nil)
+      case method
+      when :get
+        client.get(url, opts.to_h)
+      when :post
+        client.post(url, opts.to_h)
+      when :patch
+        client.patch(url, opts.to_h)
+      when :delete
+        client.delete(url, opts.to_h)
+      else
+        raise 'invalid method error'
+      end
+    rescue => e
+      puts e.message
+    end
   end
 end

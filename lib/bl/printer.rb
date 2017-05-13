bc9914a6832ad226738c653cb9824cd69760156c
saki <sakihet@gmail.com>
2017-05-13 21:10:03 +0900

    add Bl::Printer

--- lib/bl/printer.rb
+++ lib/bl/printer.rb
@@ -5,10 +5,25 @@ module Bl
     def print_response(resource, res)
       case resource
       when :issue
-        puts formatter.render(res, fields: ISSUE_FIELDS)
+        puts formatter.render(printable_issues(res.body), fields: ISSUE_FIELDS)
       else
         raise 'invalid resource error'
       end
+    end
+
+    def printable_issues(ary)
+      ary = Array(ary)
+      ary.each do |v|
+        v.issueType = v.issueType.name
+        v.assignee = v.assignee.name if v.assignee
+        v.status = v.status.name
+        v.priority = v.priority.name
+        v.startDate = format_date(v.startDate)
+        v.dueDate = format_date(v.dueDate)
+        v.created = format_datetime(v.created)
+        v.updated = format_datetime(v.updated)
+      end
+      ary
     end
   end
 end

d510b7e344b8f0d26310d2aa896c89cace819842
saki <sakihet@gmail.com>
2017-12-07 09:23:15 +0900

    add Bl::Commands::Issue

--- lib/bl/cli.rb
+++ lib/bl/cli.rb
@@ -46,46 +46,6 @@ module Bl
       end
     end
 
-    desc 'count', 'count issues'
-    options ISSUES_PARAMS
-    def count
-      puts request(:get, 'issues/count', delete_class_options(options.to_h)).body.count
-    end
-
-    desc 'list', 'list issues by typical ways'
-    option :all
-    option :unassigned
-    option :today
-    option :overdue
-    option :priority
-    option :nocategory
-    def list
-      opts = {}
-      opts[:statusId] = [1, 2, 3] unless options[:all]
-      opts[:assigneeId] = [-1] if options[:unassigned]
-      if options[:today]
-        today = Date.today
-        opts[:dueDateSince] = today.to_s
-        opts[:dueDateUntil] = today.next.to_s
-      end
-      opts[:dueDateUntil] = Date.today.to_s if options[:overdue]
-      if options[:priority]
-        opts[:sort] = 'priority'
-        opts[:order] = 'asc'
-      end
-      opts[:categoryId] = [-1] if options[:nocategory]
-      opts[:count] = ISSUES_COUNT_MAX
-      res = request(:get, 'issues', opts)
-      print_response(res, :issue)
-    end
-
-    desc 'search', 'search issues'
-    options ISSUES_PARAMS
-    def search
-      res = request(:get, 'issues', delete_class_options(options.to_h))
-      print_response(res, :issue)
-    end
-
     desc 'show KEY', "show an issue's details"
     def show(key)
       res  = request(:get, "issues/#{key}")
@@ -119,58 +79,6 @@ module Bl
     def browse(key)
       url = 'https://' + @config[:space_id] + '.backlog.jp/view/' + key
       system("open #{url}")
-    end
-
-    desc 'add [SUBJECT...]', 'add issues'
-    options ISSUE_BASE_ATTRIBUTES
-    def add(*subjects)
-      subjects.each do |s|
-        issue_default_options = @config[:issue][:default]
-        res = request(:post,
-          'issues',
-          issue_default_options.merge({summary: s}).merge(delete_class_options(options.to_h))
-        )
-        puts '💡 issue added'
-        print_response(res, :issue)
-      end
-    end
-
-    desc 'update [KEY...]', 'update issues'
-    options ISSUE_BASE_ATTRIBUTES
-    option :comment, type: :string
-    def update(*keys)
-      keys.each do |k|
-        res = request(:patch, "issues/#{k}", delete_class_options(options.to_h))
-        puts 'issue updated'
-        print_response(res, :issue)
-      end
-    end
-
-    desc 'close [KEY...]', 'close issues'
-    def close(*keys)
-      keys.each do |k|
-        res = request(:patch, "issues/#{k}", statusId: 4)
-        puts '🎉 issue closed'
-        print_response(res, :issue)
-      end
-    end
-
-    desc 'edit KEY', "edit issues' description by $EDITOR"
-    def edit(key)
-      issue_description = request(:get, "issues/#{key}").body.description
-      file = Tempfile.new
-      file.puts(issue_description)
-      file.close
-      begin
-        file.open
-        system("$EDITOR #{file.path}")
-        new_content = file.read
-        request(:patch, "issues/#{key}", description: new_content)
-        puts "issue #{key} updated."
-      ensure
-        file.close
-        file.unlink
-      end
     end
 
     desc 'statuses', 'list statuses'
@@ -207,6 +115,9 @@ module Bl
 
     desc 'group SUBCOMMAND ...ARGS', 'manage groups'
     subcommand 'group', Commands::Group
+
+    desc 'issue SUBCOMMAND ...ARGS', 'manage issues'
+    subcommand 'issue', Commands::Issue
 
     desc 'milestone SUBCOMMAND ...ARGS', 'manage milestones'
     subcommand 'milestone', Commands::Milestone
